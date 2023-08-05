2814b874be63bb5ca84a75aa2ac62a1f1b52e4c2
Yuriy Bykov <jim95022@gmail.com>
2023-08-05 17:11:37 +0400

    etended functianl test. it checks home page content

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,21 +21,38 @@ class NewVisitorTest(unittest.TestCase):
         # Ему кмдают ссылку и он переходит на домашнюю страницу.
         self.browser.get("http://localhost:8000")
 
+
         # Он обращает внимение что заголовок страницы говорят о туду списке
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Закончить тест!")
+        header_text = self.browser.find_element_by_id("h1").text
+        self.assertIn("To-Do", header_text)
 
 
         # Олегу предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
 
 
         # Олежа набирает в текстовом поле "Купить молока". (Олег готовит привосходные молочные коктейли)
+        inputbox.send_keys("Купить молока")
 
 
         # Олег нажимает Enter, страница обновляется, и теперь страница содержит первый элемент "1. Купить молока"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1. Купить молока" for row in rows)
+        )
 
 
         # Текстовое поле по-прежнему приглашает его добавить еще одно поле. Олег вводит "смешать молоко и бананаы в блендере"
+        self.fail("Закончить тест!")
 
 
         # Страница обновляется и теперь отображается два элемента в списке. 
