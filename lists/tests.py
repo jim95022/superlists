850da3118352c3caabd39046efc4836d06d603be
Yuriy Bykov <jim95022@gmail.com>
2023-08-05 16:31:53 +0400

    home page and the first moule test

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    """Тест на токсичность"""
+class HomePageTest(TestCase):
+    """Тест на домашней страницы"""
 
-    def test_bad_maths(self):
-        """Тест: неправильный математические расчеты"""
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """Тест: корневой URL преобразуется в предоставление домашней страницы"""
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
