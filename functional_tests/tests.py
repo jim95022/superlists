92bbd985e0a1c3eaf4bd6edf6d6a1e36d4195bb5
Yuriy Bykov <jim95022@gmail.com>
2023-08-12 15:50:16 +0400

    removed sleep functions

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+
+MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
     """Тест нового посетителя"""
@@ -14,10 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         """Демонтаж"""
         self.browser.quit()
 
-    def check_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """Ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(1/2)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно начать новый список и получить его позже"""
@@ -47,20 +61,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Олег нажимает Enter, страница обновляется, и теперь страница содержит первый элемент "1. Купить молока"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_row_in_list_table("1. Купить молока")
+        self.wait_for_row_in_list_table("1. Купить молока")
 
 
         # Текстовое поле по-прежнему приглашает его добавить еще одно поле. Олег вводит "смешать молоко и бананаы в блендере"
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Cмешать молоко и бананаы в блендере")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
 
         # Страница обновляется и теперь отображается два элемента в списке. 
-        self.check_row_in_list_table("1. Купить молока")
-        self.check_row_in_list_table("2. Cмешать молоко и бананаы в блендере")
+        self.wait_for_row_in_list_table("1. Купить молока")
+        self.wait_for_row_in_list_table("2. Cмешать молоко и бананаы в блендере")
 
 
         # Олегу интересно останится ли запись после того как он закроет сайт. 
