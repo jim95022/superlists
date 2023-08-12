70c6120c8c5ab4ff6ac7244c34acc7546c701b3f
Yuriy Bykov <jim95022@gmail.com>
2023-08-12 15:06:34 +0400

    redirection after POST and all items in the template

--- lists/tests.py
+++ lists/tests.py
@@ -11,18 +11,45 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         """Тест: корневой URL преобразуется в предоставление домашней страницы"""
         found = resolve("/")
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         """Тест: домашняя страница возвращает правильный html"""
         response = self.client.get("/")
+
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self):
         """Тест: можно сохранить post-запрос"""
+        self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        """Тест: переадресует после post-запроса"""
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("a new list item", response.content.decode())
-        self.assertTemplateUsed(response, "home.html")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_only_saves_items_when_necessary(self):
+        """Тест: сохраняет элементы, только когда нужно"""
+        self.client.get("/")
+        
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        """Тест: отображаются все элементы списка"""
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
 
 
 class ItemModelTest(TestCase):
