34e2c9e8b59fc64260922c9f9ed0dd15d03c0b04
Yuriy Bykov <jim95022@gmail.com>
2023-08-12 17:27:47 +0400

    splitted tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,8 +33,8 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(1/2)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """Тест: можно начать новый список и получить его позже"""
+    def test_can_start_a_list_for_one_user(self):
+        """Тест: можно начать список для одного пользователя"""
 
         # Олег узнал про новое приложение для списков дел.
         # Ему кмдают ссылку и он переходит на домашнюю страницу.
@@ -75,14 +75,60 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table("2. Cмешать молоко и бананаы в блендере")
 
 
+        # Олег закрывает браузер и идет в магазин.
+        self.browser.quit()
+
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """Тест: многочисленные пользователи могут начать списки по разным url"""
+        
+        # Олег начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Купить молока")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Купить молока")
+
+
         # Олегу интересно останится ли запись после того как он закроет сайт. 
         # Он обращает внимание что сайт сгенерировал уникальный URL адрес. 
         # Также присутсует небольшой текст с объснением
-        self.fail("Закончить тест!")
+        oleg_list_url = self.browser.current_url
+        self.assertRegex(oleg_list_url, r"/lists/.+")
 
 
-        # Олег снова посещает этот уникальный URL адрес, список по прежнему там.
+        # Теперь приходит новый пользователь, Руслан
 
-
-        # Олег закрывает браузер и идет в магазин.
+        ## Мы создаем новый сеанс браузера, чтобы обеспечить изолированность.
+        ## Никакая инофрмация от Олега не должна остаться. Включая куки и тд.
         self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+
+        # Руслан открывает домашнюю страницу. Нет никаких признаков списка Олега.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Купить молока", page_text)
+        self.assertNotIn("Cмешать молоко и бананаы в блендере", page_text)
+
+
+        # Руслан начинает свой список. вводня новый элемент.
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Заказать протеин")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Заказать протеин")
+
+
+        # Руслан получает уникальный URL адрес
+        ruslan_list_url = self.browser.current_url
+        self.assertRegex(ruslan_list_url, r"/lists/.+")
+        self.assertNotEqual(ruslan_list_url, oleg_list_url)
+
+
+        # Опять таки нет следа от Олега
+        page_text = self.browser.find_element_by_name("body").text
+        self.assertNotIn("Купить молока", page_text)
+        self.assertNotIn("Cмешать молоко и бананаы в блендере", page_text)
+
+
+        # Оба закрывают странцу и ложаться спать. 
