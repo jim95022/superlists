60372ef1d14363eabc0192053c6818eac087a2a4
Yurii Bykov <yuri.bikov@metamap.com>
2023-11-23 20:55:55 +0400

    added bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +8,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """Тест нового посетителя"""
 
 
@@ -156,10 +156,10 @@ class NewVisitorTest(LiveServerTestCase):
         # Аккуратно центировано
         inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: testing")
+        self.wait_for_row_in_list_table("1. testing")
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
             512,
-            10
+            delta=10
         )
