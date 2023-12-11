eac5ca391572337d0849228b080377e25092727c
Yurii <jim95022@gmail.com>
2023-12-11 18:55:52 +0400

    removed deblicated url

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -42,3 +42,8 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "Item the second")
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_get_absolute_url(self):
+        """Тест: получен абсолютный url"""
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
