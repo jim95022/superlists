5d7e0ffb2db99f274dbebd25f387b4ec84583d31
Yurii <jim95022@gmail.com>
2023-12-21 14:54:29 +0400

    new qunit tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,10 @@ from functional_tests.base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
     """Тест валидации элемента списка"""
 
+    def get_error_element(self):
+        """получить элемент с ошибкой"""
+        return self.browser.find_element_by_css_selector(".has-error")
+
     def test_cannot_add_empty_list_items(self):
         """Тест: нельзя добавлять пустые элементы списка"""
         # Олег открывает домашнюю страницу и случайно пытается отправить пустой элемент списка.
@@ -52,7 +56,25 @@ class ItemValidationTest(FunctionalTest):
         # Он видит полезное сообщение об ошибке
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text,
+                self.get_error_element().text,
                 "You've already got this in your list"
             )
         )
+
+    def test_error_messages_are_cleared_on_input(self):
+        """Тест: сообщения об ошибках очищаются при вводе"""
+        # Олег начинает список и вызывает ошибку валидации
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Banter too thick")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Banter too thick")
+        self.get_item_input_box().send_keys("Banter too thick")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(self.get_error_element().is_displayed()))
+
+        # Он начинает набирать в поле ввода, чтобы очистить ошибку
+        self.get_item_input_box().send_keys("a")
+
+        # Он рад что сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(self.get_error_element().is_displayed()))
