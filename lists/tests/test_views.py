67fb0c8a0cd9fc6739cec8b2fdda3b6e9defe570
Yurii <jim95022@gmail.com>
2023-12-21 14:33:09 +0400

    qunit tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -25,6 +25,14 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
     """Тест представления списка"""
 
+    def post_invalid_input(self):
+        """Отправляет недопустимый ввод"""
+        list_ = List.objects.create()
+        return self.client.post(
+            f"/lists/{list_.id}/",
+            data={"text": ""}
+        )
+
     def test_uses_list_template(self):
         """Тест: используется шаблон списка"""
         mylist = List.objects.create()
@@ -37,6 +45,13 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
+
+    def test_displays_item_form(self):
+        """Тест отображения формы для элемента"""
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
+        self.assertContains(response, "name=\"text\"")
 
     def test_displays_only_items_for_that_list(self):
         """Тест: отображаются элементы только для этого списка"""
@@ -81,14 +96,6 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-    def post_invalid_input(self):
-        """Отправляет недопустимый ввод"""
-        list_ = List.objects.create()
-        return self.client.post(
-            f"/lists/{list_.id}/",
-            data={"text": ""}
-        )
-
     def test_for_invalid_input_nothing_saved_to_db(self):
         """Тест на недопустимый ввод: ничего не сохраняется в БД"""
         self.post_invalid_input()
@@ -99,6 +106,11 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "list.html")
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        """Тест на недопустимый ввод: форма передается в шаблон"""
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         """Тест на недопустимый ввод: на странице показывается ошибка"""
@@ -119,13 +131,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, "list.html")
         self.assertEqual(Item.objects.all().count(), 1)
-
-    def test_displays_item_form(self):
-        """Тест отображения формы для элемента"""
-        list_ = List.objects.create()
-        response = self.client.get(f"/lists/{list_.id}/")
-        self.assertIsInstance(response.context["form"], ExistingListItemForm)
-        self.assertContains(response, "name=\"text\"")
 
     def test_invalid_list_items_arent_saved(self):
         """Тест: сохраняются недопустимые элементы списка"""
@@ -157,8 +162,3 @@ class ListViewTest(TestCase):
         """Тест: ошибки валидации выводятся на домашней странице"""
         respnose = self.client.post("/lists/new", data={"text": ""})
         self.assertContains(respnose, escape(EMPTY_ITEM_ERROR))
-
-    def test_for_invalid_input_passes_form_to_template(self):
-        """Тест на недопустимый ввод: форма передается в шаблон"""
-        response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ExistingListItemForm)
