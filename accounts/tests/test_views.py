4b7459065562b29d04f9ca099995988fd505376d
Yurii <jim95022@gmail.com>
2023-12-22 16:47:18 +0400

    email sending logic

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from unittest.mock import patch
 import accounts.views
+from accounts.models import Token
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -7,33 +9,66 @@ class SendLoginEmailViewTest(TestCase):
 
     def test_redirects_to_home_page(self):
         """Тест: переадресуется на домашнюю страницу"""
-        response = self.client.post(
-            "/accounts/send_login_email",
-            data={
-                "email": "jim9502@example.com"
-            }
-        )
+        # response = self.client.post(
+        #     "/accounts/send_login_email",
+        #     data={
+        #         "email": "jim9502@example.com"
+        #     }
+        # )
+        response = self.client.get("/accounts/login?token=abcd123")
         self.assertRedirects(response, "/")
 
-    def test_sends_mail_to_address_from_post(self):
+    @patch("accounts.views.send_mail")
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
         """Тест: отправляется сообщение на адрес из метода post"""
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            """Поддельная функция send_mail"""
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
 
         self.client.post("/accounts/send_login_email", data={
             "email": "jim9502@example.com"
         })
 
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, "Your login link for Superlists")
-        self.assertEqual(self.from_email, "noreply@superlists")
-        self.assertEqual(self.to_list, ["jim9502@example.com"])
+        self.assertTrue(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+
+        self.assertEqual(subject, "Your login link for Superlists")
+        self.assertEqual(from_email, "noreply@superlists")
+        self.assertEqual(to_list, ["jim9502@example.com"])
+
+    def test_adds_success_message(self):
+        """Тест: добавляется сообщение об успехе"""
+        response = self.client.post(
+            "/accounts/send_login_email",
+            data={
+                "email": "jim9502@exaple.com"
+            },
+            follow=True
+        )
+
+        message = list(response.context["messages"])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email",
+        )
+        self.assertEqual(message.tags, "success")
+
+    def test_creates_token_associated_with_email(self):
+        """Тест: создается маркер, связанный с электронной почтой"""
+        self.client.post("/accounts/send_login_email", data={
+            "email": "jim9502@example.com"
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, "jim9502@example.com")
+
+    @patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        """Тест: отсылается ссылка на вход в систему, используя uid маркера"""
+        self.client.post(
+            "/accounts/send_login_email",
+            data={
+                "email": "jim9502@gmail.com"
+            }
+        )
+
+        token = Token.objects.first()
+        expected_url = f"http://testserver/accounts/login?token={token.uid}"
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
