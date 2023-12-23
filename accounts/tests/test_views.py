09b38e99492e5c4f68401a0d68615b855a5c4fd6
Yurii <jim95022@gmail.com>
2023-12-23 23:22:18 +0400

    new tests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -75,3 +75,12 @@ class SendLoginEmailViewTest(TestCase):
             mock_auth.authenticate.call_args,
             call(uid="abcd123")
         )
+
+    @patch("accounts.views.auth")
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        """Тест: вызывается auth_login с пользователем, если такой имеется"""
+        response = self.client.get("/accounts/login?token=abcd123")
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
