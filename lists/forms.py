8de85aae91e41cc9dbf9190953cfce8362841fed
Yurii Bykov <53302191+jim95022@users.noreply.github.com>
2024-01-03 23:04:49 +0400

    Update forms.py

--- lists/forms.py
+++ lists/forms.py
@@ -4,8 +4,10 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 
+
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
+
 
 
 class ItemForm(forms.ModelForm):
