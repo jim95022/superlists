57ed37f332647199881bc18332ba4e3b6633b449
Yurii Bykov <53302191+jim95022@users.noreply.github.com>
2024-01-07 23:39:15 +0400

    Update models.py

--- lists/models.py
+++ lists/models.py
@@ -12,6 +12,7 @@ class List(models.Model):
 
 class Item(models.Model):
     """Элемент списка"""
+
     text = models.TextField(default="")
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
